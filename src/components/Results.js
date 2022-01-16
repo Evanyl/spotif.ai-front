8dd75a76efc2bb3a7be72c2633257a976c828817
Evan <liuyievan@gmail.com>
2022-01-16 04:49:27 -0800

    changes

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,17 +1,28 @@
 import handleSongs from '../utils/GetSongs';
+import postPlaylist from '../utils/PostPlaylist';
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpotify } from '@fortawesome/free-brands-svg-icons';
 
 const Results = (props) => {
     const [items, setItems] = useState([]);
+    const [created, setCreated] = useState(false);
+    const button = "flex max-w-xs items-center justify-center text-base font-medium rounded-md text-black bg-green-300 hover:bg-green-400 px-8 py-3 md:py-4 md:text-lg md:px-10";
 
     useEffect(() => {
         handleSongs(props.prompt, props.token, setItems);
     }, [props.prompt]);
 
+    const createPlaylist = () => {
+        setCreated(true)
+
+        postPlaylist(props.prompt, props.token, items);
+    }
 
     return (
         <div className="flex flex-col items-center">
             <h2 className="text-2xl p-4 uppercase font-extrabold">{props.prompt}</h2>
+            {!created && <button onClick={createPlaylist} className={button}><FontAwesomeIcon icon={faSpotify} size="lg"/> <div className="px-1"></div>Create Playlist</button>}
             <div className="p-2" />
             <div className="flex flex-col w-full max-w-2xl border-t">
                 {items.map((item, index) => {

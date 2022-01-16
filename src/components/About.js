48a93badfc96ce521d65e92d2c3adf85f3bf33c1
Evan <liuyievan@gmail.com>
2022-01-15 20:57:24 -0800

    About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,22 @@
-import { faMicrochip } from "@fortawesome/free-solid-svg-icons";
+import { faMicrochip, faMusic, faParagraph } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const About = (props) => {
     return (
         <div className="flex flex-col w-full">
             <h2 className="text-5xl font-black text-left pt-8">How it works</h2>
-            <div className="flex w-full items-center justify-center pt-2">
-                <div className="flex flex-col items-center justify-center w-1/3 pt-2">
-                    <FontAwesomeIcon icon={faMicrochip} size="4x" className="text-green-300"/> 
-                    <p className="pt-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className="flex w-full items-start justify-between pt-4">
+                <div className="flex flex-col items-center justify-center w-1/4 p-2">
+                    <FontAwesomeIcon icon={faParagraph} size="5x" className="text-green-300"/> 
+                    <p className="pt-4 text-center text-sm">You will enter a phrase you want to see in song form.</p>
                 </div>
-                <div className="flex flex-col items-center justify-center w-1/3 pt-2">
-                    <FontAwesomeIcon icon={faMicrochip} size="4x" className="text-green-300"/> 
-                    <p className="pt-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className="flex flex-col items-center justify-center w-1/4 p-2">
+                    <FontAwesomeIcon icon={faMicrochip} size="5x" className="text-green-300"/> 
+                    <p className="pt-4 text-center text-sm">Our machine learning algorithm finds the best song choices for you.</p>
                 </div>
-                <div className="flex flex-col items-center justify-center w-1/3 pt-2">
-                    <FontAwesomeIcon icon={faMicrochip} size="4x" className="text-green-300"/> 
-                    <p className="pt-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className="flex flex-col items-center justify-center w-1/4 p-2">
+                    <FontAwesomeIcon icon={faMusic} size="5x" className="text-green-300"/> 
+                    <p className="pt-4 text-center text-sm">See your song choices and save them to Spotify!</p>
                 </div>
             </div>
         </div>
